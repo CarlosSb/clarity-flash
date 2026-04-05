dde271ce79b5af36fe661a8134f6dca165b9a822
CarlosSb <antoniocarlossbcdd@gmail.com>
2026-04-05 10:01:21 -0300

    refactor: remove old domain services
    - Remove flashcard, summary, and transcription services
    - Remove deprecated router and cache components
    - Prepare for new processing architecture

--- backend/internal/domain/service/flashcard.go
+++
@@ -1,9 +0,0 @@
-package service
-
-// FlashcardService contem a logica de geracao de flashcards
-type FlashcardService struct{}
-
-// NewFlashcardService cria uma nova instancia
-func NewFlashcardService() *FlashcardService {
-	return &FlashcardService{}
-}
--- backend/internal/domain/service/summary.go
+++
@@ -1,9 +0,0 @@
-package service
-
-// SummaryService contem a logica de geracao de resumos
-type SummaryService struct{}
-
-// NewSummaryService cria uma nova instancia
-func NewSummaryService() *SummaryService {
-	return &SummaryService{}
-}
